ebbe6ee27952639198904c0abb1a1c9bc2352d0d
rahulsinha1212 <fakelying@gmail.com>
2017-01-09 18:13:14 +0530

    Add files via upload

---
+++ curl_php.php
@@ -0,0 +1,57 @@
+<div style="background:url(123.png) no-repeat red;height:100px"><img src="1234.png"></div>
+<?php
+// $curl = curl_init();
+// // Set some options - we are passing in a useragent too here
+// curl_setopt_array($curl, array(
+    // CURLOPT_RETURNTRANSFER => 1,
+    // CURLOPT_URL => 'http://192.168.1.101/onebharat/auth/registration',
+    // CURLOPT_USERAGENT => 'Codular Sample cURL Request',
+    // CURLOPT_POST => 1,
+    // CURLOPT_POSTFIELDS => array(
+        // 'firstname' => 'value',
+        // 'mothername' => 'value2'
+    // )
+// ));
+// // Send the request & save response to $resp
+// $resp = curl_exec($curl);
+// // Close request to clear up some resources
+// curl_close($curl);
+
+// function httpPost($url,$params){
+  // $postData = '';
+   // //create name value pairs seperated by &
+   // foreach($params as $k => $v) { 
+      // $postData .= $k . '='.$v.'&'; 
+   // }
+   // $postData = rtrim($postData, '&');
+    // $ch = curl_init();  
+    // curl_setopt($ch,CURLOPT_URL,$url);
+    // curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    // curl_setopt($ch,CURLOPT_HEADER, false); 
+    // curl_setopt($ch, CURLOPT_POST, count($postData));
+    // curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);    
+    // $output=curl_exec($ch);
+    // curl_close($ch);
+    // return $output;
+// }
+
+// $params = array(
+	// "friend_id" => '1',
+	// "post_id" => '3',
+	// "comment" => 'my name is rahul',
+	// "p_id" => 0
+// );
+
+// $params1 = array(
+   // "firstname" => "rahul",
+   // "mothername" => "aarti"
+// );
+// while(1){
+	// echo httpPost("http://192.168.1.103/onebharat/auth/registration",$params1);
+// }
+// while(1){
+// echo httpPost("http://192.168.1.103/onebharat/friend/comments_post",$params);
+// }
+//echo httpPost("http://192.168.1.101/onebharat/auth/create_user",$params);
+
+?>
